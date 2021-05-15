56cf98572c3678aec1c57818d1c2fff322e61bbb
A Prashanth Reddy <63457391+PrashanthreddyAinala@users.noreply.github.com>
2021-05-15 15:11:42 +0530

    First commit

---
+++ src/components/MoviesList.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react";
+import Movie from "./movie"
+
+const APIUrl ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const searchUrl = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+
+function MoviesList() {
+    const [movies, setMovies] = useState([])
+    const [inputSearch, setInputSearch] = useState('')  
+    
+    useEffect(()=>{
+        fetch(APIUrl)
+          .then(response=> response.json())
+          .then(data=>setMovies(data.results));
+    }, [])
+    
+    function handleSubmit (e) {
+        e.preventDefault();
+        fetch(searchUrl + inputSearch )
+            .then(response=> response.json())
+            .then(data=>setMovies(data.results));
+        setInputSearch("")
+    }
+    
+    return (
+        <>
+            <div className="grid">
+                <h1 className="header">MyMovies</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={inputSearch} placeholder="search" onChange={e=>setInputSearch(e.target.value)}/>
+                </form>
+            </div>
+            <div className="container">
+                <div className="row">
+                    {movies.length > 0 && movies.map(movie=><Movie key={movie.id} {...movie}/>)}
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MoviesList;
---
+++ src/components/movie.js
@@ -0,0 +1,22 @@
+import React from "react";
+const imgUrl = "https://image.tmdb.org/t/p/w1280";
+
+function Movie ({title, original_title , overview, poster_path, vote_average}) {
+    
+    return (
+        <div className="col-12 col-md-6 col-lg-3">
+            <div className="card mb-4" >
+                <img className="card-img-top" src={imgUrl + poster_path} alt="poster" />
+                <div className="card-body">
+                    <p className="title">{title}</p>
+                    <p className="vote">{vote_average}</p>
+                </div>
+                <div className="overview">
+                    <h5>Overview</h5>
+                    <p>{overview}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Movie;
